d460ec8d7807fa7f1653f20572a9bc04eb9d8cf6
UP2017778 <sking233952@gmail.com>
2021-12-21 18:19:57 +0000

    Completed disdection of the array/ added color editor

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 const textArea = document.querySelector("#userInput");
+const colorPicker = document.querySelector("#colorPicker")
 
 window.onload = () => {
     count(textArea)
@@ -8,12 +9,16 @@ textArea.addEventListener("input", ({ currentTarget: target }) => {
     count(target)
 })
 
+colorPicker.addEventListener("input", ({ currentTarget: target }) => {
+    document.querySelector("#colorResult").innerHTML = target.value
+})
+
+
 function count(target) {
     const maxChar = target.getAttribute("maxlength");
     const currentChar = target.value.length;
 
     const count = document.querySelector("#charCount")
-    const showcase = document.querySelector("#showcase")
 
     if (currentChar >= maxChar) {
         return count.innerHTML = `Character Count: None remaining`
@@ -21,12 +26,35 @@ function count(target) {
 
     let output = () => {
         count.innerHTML = `Character Count: ${maxChar - currentChar} remaining`;
-        showcase.innerHTML = target.value;
+        preview(target)
     }
 
     return output()
 }
 
-//function JSONReplace()
+function preview(content) {
+    const showcase = document.querySelector("#showcase")
+    const regex = "\{[^()]+?\}";
 
-//Regex = \{[^()]+?\}
+    const unformattedText = content.value.split(" ");
+    let position = 0;
+
+    for (const element of unformattedText) {
+        if (element.match(regex)) {
+            unformattedText[position] = "<b> test </b>"
+        }
+        position++;
+    }
+
+
+    
+    return showcase.value = unformattedText.join(" ")
+}
+
+
+/*
+    result = colorArray.length > 0 ?
+        result = colorArray.map(color => {
+            return content.value.replaceAll(color, "test")
+        }) : content.value;
+*/
